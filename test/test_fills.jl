3005cf10eb3bbe6653bcfe4d036630d27ca7ff0f
Olivier Milla <olivier.milla@hey.com>
2024-02-07 14:11:41 +0100

    Added a test for Fills

--- test/test_fills.jl
+++ test/test_fills.jl
@@ -1,5 +1,8 @@
 @testset "Fills" begin
-    @testset "Constructors" begin
-        @test_skip "PENDING" 
+    instr = Cash(:USD)
+    order = MarketOrder(instr, 1.0)
+    @testset "Constructors" begin                
+        fill = Fill("id", order, 12.45, 1.0, 0.0, Dates.now(Dates.UTC))
+        @test fill isa Fill{MarketOrder{Cash{Currency{:USD}}}}
     end
 end
