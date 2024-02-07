8bc007f3f78d17acd129d9ba924880ff45276391
Olivier Milla <olivier.milla@hey.com>
2024-02-07 14:11:48 +0100

    More coverage

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,11 +10,12 @@ include("test_currencies.jl")
 include("test_instruments.jl")
 include("test_quotes.jl")
 include("test_positions.jl")
+include("test_orders.jl")
+include("test_fills.jl")
 
 include("test_ohlcs.jl")
 include("samplers/test_ohlc_samplers.jl")
 
-include("test_orders.jl")
 include("exchanges/test_fake_exchanges.jl")
 
 # Extensions
