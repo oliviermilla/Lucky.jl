bdb3d188e3fef2f7c27855fd5605b8c6af9d0fac
Olivier Milla <olivier.milla@hey.com>
2024-02-07 14:12:04 +0100

    Implemented on_next! for InMemoryBlotter

--- src/blotters/InMemoryBlotters.jl
+++ src/blotters/InMemoryBlotters.jl
@@ -3,14 +3,22 @@ module InMemoryBlotters
 export InMemoryBlotter
 
 using Lucky.Blotters
+using Lucky.Fills
 
 using Rocket
 
 struct InMemoryBlotter <: AbstractBlotter
-    positions::Vector{Any}
+    fills::Vector{Fill} # TODO Optimize
     next::AbstractSubject
 end
-@inline InMemoryBlotter(subject::Subject) = InMemoryBlotter(Vector{Any}(), subject)
+@inline InMemoryBlotter(subject::Subject) = InMemoryBlotter(Vector{Fill}(), subject)
+
+function Rocket.on_next!(actor::InMemoryBlotter, fill::Fill)
+    push!(actor.fills, fill)
+    aggSize = mapreduce(x -> x.instrument == fill.instrument ? fill.size : zero(Float64), +, actor.fills)
+    position = Position(fill.instrument, aggSize)
+    next!(actor.next, position)
+end
 
 Rocket.on_error!(actor::InMemoryBlotter, error) = error!(actor.next, error)
 Rocket.on_complete!(actor::InMemoryBlotter) = complete!(actor.next)
