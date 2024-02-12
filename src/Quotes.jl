e5b33df85c3be9e514a6dcd70a3a7d24d6aa91b4
Olivier Milla <olivier.milla@hey.com>
2024-02-12 16:27:49 +0100

    Added operators for quotes

--- src/Quotes.jl
+++ src/Quotes.jl
@@ -1,6 +1,6 @@
 module Quotes
 
-export AbstractQuote, NumberQuote, OhlcQuote
+export AbstractQuote, PriceQuote, OhlcQuote
 export Quote
 export currency, timestamp
 
@@ -12,7 +12,7 @@ using Dates
 
 abstract type AbstractQuote end
 
-struct NumberQuote{I,Q,D} <: AbstractQuote
+struct PriceQuote{I,Q,D} <: AbstractQuote
     instrument::I
     price::Q
     timestamp::D
@@ -24,11 +24,32 @@ struct OhlcQuote{I} <: AbstractQuote
 end
 
 # Interfaces
-Quote(instrument::Instrument, price::Q, stamp::D) where {Q<:Number,D<:Dates.AbstractTime} = NumberQuote(instrument, price, stamp)
+Quote(instrument::Instrument, price::Q, stamp::D) where {Q<:Number,D<:Dates.AbstractTime} = PriceQuote(instrument, price, stamp)
 Quote(instrument::Instrument, ohlc::Q) where {Q<:Ohlc} = OhlcQuote(instrument, ohlc)
 
 Units.currency(q::AbstractQuote) = Units.currency(q.instrument)
 timestamp(q::OhlcQuote) = q.ohlc.timestamp
-timestamp(q::NumberQuote) = q.timestamp
+timestamp(q::PriceQuote) = q.timestamp
+
+import Base: +, -, *, /, convert
+# https://github.com/JuliaLang/julia/blob/0a8916446b782eae1a09681b2b47c1be26fab7f3/base/missing.jl#L119
+for f in (:(+), :(-)) #, :(*), :(/), :(^), :(mod), :(rem))
+    @eval begin
+        ($f)(::Missing, ::AbstractQuote) = missing
+        ($f)(::AbstractQuote, ::Missing) = missing
+    end
+end
+
++(x::I, y::I) where {I<:PriceQuote} = I(x.instrument, x.price + y.price, max(timestamp(x), timestamp(y)))
+-(x::I, y::I) where {I<:PriceQuote} = I(x.instrument, x.price - y.price, max(timestamp(x), timestamp(y)))
+*(x::I, y::N) where {I<:PriceQuote, N<:Number} = I(x.instrument, x.price * y, timestamp(x))
+/(x::I, y::N) where {I<:PriceQuote, N<:Number} = I(x.instrument, x.price / y, timestamp(x))
+convert(T::Type{<:Number}, x::PriceQuote) = convert(T, x.price)
+
++(x::I, y::I) where {I<:OhlcQuote} = I(x.instrument, x.ohlc + y.ohlc)
+-(x::I, y::I) where {I<:OhlcQuote} = I(x.instrument, x.ohlc - y.ohlc)
+*(x::I, y::N) where {I<:OhlcQuote, N<:Number} = I(x.instrument, x.ohlc * y)
+/(x::I, y::N) where {I<:OhlcQuote, N<:Number} = I(x.instrument, x.ohlc / y)
+convert(T::Type{<:Number}, x::OhlcQuote) = convert(T, x.ohlc)
 
 end
--- test/test_quotes.jl
+++ test/test_quotes.jl
@@ -3,15 +3,34 @@
     @testset "Number" begin
         price = 51.7
         stamp = Date(2021, 1, 1)
-        q = Quote(stock, price, stamp)
+        q1 = Quote(stock, price, stamp)
         @testset "Constructors" begin
-            @test q isa Lucky.Quotes.NumberQuote
-            @test q.instrument == stock
-            @test q.price == price
+            @test q1 isa Lucky.Quotes.PriceQuote
+            @test q1.instrument == stock
+            @test q1.price == price
         end
-        @testset "Interface" begin
-            currency(q) == Currency{:USD}
-            timestamp(q) == timestamp
+        @testset "Interfaces" begin
+            currency(q1) == Currency{:USD}
+            timestamp(q1) == timestamp
+        end
+        @testset "Operators" begin
+            p2 = 8.3
+            t2 = Date(2021, 2, 1)
+            q2 = Quote(stock, p2, t2)
+
+            # Valid ops
+            @test q1 + q2 == Quote(stock, price + p2, t2)
+            @test q2 - q1 == Quote(stock, p2 - price, t2)
+
+            # Ops with missing
+            @test q1 + missing === missing
+            @test missing - q1 === missing
+
+            # Different instruments
+            cash = Cash(:USD)
+            q3 = Quote(cash, p2, t2)
+            @test_throws MethodError q1 + q3
+            @test_throws MethodError q3 - q1
         end
     end
     @testset "Ohlc" begin
