2eecbc7fcca98b082d50552149eb294071b93aef
Olivier Milla <olivier.milla@hey.com>
2024-02-13 19:21:15 +0100

    Added isless for quotes and Ohlcs

--- src/Ohlcs.jl
+++ src/Ohlcs.jl
@@ -42,13 +42,14 @@ end
 end
 
 # Operators
-import Base: +, -, *, /, convert
+import Base: +, -, *, /, convert, isless
 +(x::I, y::I) where {I<:Ohlc} = I(x.open, max(x.high, y.high), min(x.low, y.low), y.close, max(x.timestamp, y.timestamp))
 #-(x::I, y::I) where {I<:Ohlc} = I(x.ohlc - y.ohlc)
 *(x::I, y::N) where {I<:Ohlc,N<:Number} = I(x.open * y, x.high * y, x.low * y, x.close * y, x.timestamp)
 /(x::I, y::N) where {I<:Ohlc,N<:Number} = I(x.open / y, x.high / y, x.low / y, x.close / y, x.timestamp)
 # Convert on close
 convert(T::Type{<:Number}, x::Ohlc) = convert(T, x.close)
+isless(x::I, y::I) where {I<:Ohlc} = isless(x.close, y.close)
 
 # Rocket stuffs
 Rocket.scalarness(::Type{<:Ohlc{T}}) where {T} = Rocket.Scalar()
--- src/Quotes.jl
+++ src/Quotes.jl
@@ -22,9 +22,9 @@ struct PriceQuote{I,Q,D} <: AbstractQuote
     timestamp::D
 end
 
-struct OhlcQuote{I} <: AbstractQuote
+struct OhlcQuote{I,Q} <: AbstractQuote
     instrument::I
-    ohlc::Ohlc
+    ohlc::Q
 end
 
 # Interfaces
@@ -40,7 +40,7 @@ Units.currency(q::AbstractQuote) = Units.currency(q.instrument)
 timestamp(q::OhlcQuote) = q.ohlc.timestamp
 timestamp(q::PriceQuote) = q.timestamp
 
-import Base: +, -, *, /, convert
+import Base: +, -, *, /, convert, isless
 # https://github.com/JuliaLang/julia/blob/0a8916446b782eae1a09681b2b47c1be26fab7f3/base/missing.jl#L119
 for f in (:(+), :(-)) #, :(*), :(/), :(^), :(mod), :(rem))
     @eval begin
@@ -54,11 +54,13 @@ end
 *(x::I, y::N) where {I<:PriceQuote,N<:Number} = I(x.instrument, x.price * y, timestamp(x))
 /(x::I, y::N) where {I<:PriceQuote,N<:Number} = I(x.instrument, x.price / y, timestamp(x))
 convert(T::Type{<:Number}, x::PriceQuote) = convert(T, x.price)
+isless(x::I, y::I) where {I<:PriceQuote} = isless(x.price, y.price)
 
 +(x::I, y::I) where {I<:OhlcQuote} = I(x.instrument, x.ohlc + y.ohlc)
 -(x::I, y::I) where {I<:OhlcQuote} = I(x.instrument, x.ohlc - y.ohlc)
 *(x::I, y::N) where {I<:OhlcQuote,N<:Number} = I(x.instrument, x.ohlc * y)
 /(x::I, y::N) where {I<:OhlcQuote,N<:Number} = I(x.instrument, x.ohlc / y)
 convert(T::Type{<:Number}, x::OhlcQuote) = convert(T, x.ohlc)
+isless(x::I, y::I) where {I<:OhlcQuote} = isless(x.ohlc, y.ohlc)
 
 end
