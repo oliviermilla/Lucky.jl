49d704812a195bf074ce734563da83012080f2fc
Olivier Milla <olivier.milla@hey.com>
2024-02-13 09:49:17 +0100

    More coverage

--- src/Indicators.jl
+++ src/Indicators.jl
@@ -12,9 +12,12 @@ Base.:-(x::I, y::I) where {I<:AbstractIndicator} = I(x.value - y.value)
 Base.isless(x::I, y::I) where {I<:AbstractIndicator} = isless(x.value, y.value)
 
 struct SMAIndicator{LT, T} <: AbstractIndicator
-    value::Union{Missing,T}
+    value::Union{Missing,T}    
 end
-SMAIndicator(length::Integer, value::Union{Missing, Number}=missing)=SMAIndicator{Val(length),typeof(value)}(value)
+function SMAIndicator(length::Integer, value::Union{Missing, Number}=missing)
+    length > 0 || error("SMAIndicator: $(length) must be positive to calculate a moving average.")
+    return SMAIndicator{Val(length),typeof(value)}(value)
+end
 
 Base.isless(x::SMAIndicator{I}, y::SMAIndicator{J}) where {I,J} = isless(x.value, y.value)
 
--- test/test_indicators.jl
+++ test/test_indicators.jl
@@ -1,13 +1,20 @@
 @testset "Indicators" begin
     @testset "SMAIndicator" begin
         @testset "Constructors" begin
-        ind = SMAIndicator(3)
-        @test ind isa SMAIndicator{Val(3),Missing}
-        @test ind.value === missing
+            ind = SMAIndicator(3)
+            @test ind isa SMAIndicator{Val(3),Missing}
+            @test ind.value === missing
 
-        ind = SMAIndicator(5, one(Float16))
-        @test ind isa SMAIndicator{Val(5),Float16}
-        @test ind.value == one(Float16)
+            ind = SMAIndicator(5, one(Float16))
+            @test ind isa SMAIndicator{Val(5),Float16}
+            @test ind.value == one(Float16)
+
+            @test_throws ErrorException SMAIndicator(0)
+        end
+        @testset "Operators" begin
+            ind = SMAIndicator(3, 57.4)
+            ind2 = SMAIndicator(6, 65.2)
+            @test ind < ind2
         end
     end
 end
