4565dfb08038566f67d90df4ece2e9f1de3df0e5
Olivier Milla <olivier.milla@hey.com>
2024-03-29 22:16:09 +0100

    Fixed EMAIndicator by introducting a new abstract type for periodic indicators.

--- src/indicators/SMAIndicators.jl
+++ src/indicators/SMAIndicators.jl
@@ -1,13 +1,13 @@
 export SMAIndicator
 
-struct SMAIndicator{LT,U,V} <: ValueIndicator{V}
+struct SMAIndicator{LT,U,V} <: PeriodicValueIndicator{LT,V}
     value::U
 end
 
-function SMAIndicator(length::Integer, value::V) where {V}
-    length > 0 || error("SMAIndicator: $(length) must be positive to calculate a moving average.")
-    return SMAIndicator{Val(length),Union{Missing,V},V}(value)
+function SMAIndicator(period::Integer, value::V) where {V}
+    period > 0 || error("SMAIndicator: $(period) must be positive to calculate a moving average.")
+    return SMAIndicator{period,Union{Missing,V},V}(value)
 end
 
 # mean returns a float when given integers
-IndicatorType(::Type{SMAIndicator}, length::Integer, V::Type{<:Integer}) = SMAIndicator{Val(length),Union{Missing,Float64},Float64}
+IndicatorType(::Type{SMAIndicator}, period::Integer, V::Type{<:Integer}) = SMAIndicator{period,Union{Missing,Float64},Float64}
