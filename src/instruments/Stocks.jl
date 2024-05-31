e4910467fddb44e94b780df57c536d106c1b514c
Olivier Milla <olivier.milla@hey.com>
2024-05-31 23:04:54 +0200

    Formatting

--- src/instruments/Stocks.jl
+++ src/instruments/Stocks.jl
@@ -5,4 +5,4 @@ struct Stock{S,C} <: Instrument end
 Stock(S::Symbol, C::Type{<:Units.Currency}) = Stock{S,C}()
 Stock(S::Symbol, C::Union{Symbol, AbstractString}) = Stock{S,Units.CurrencyType(C)}()
 
-Units.currency(s::Stock{S,C}) where {S,C} = Units.CurrencyType(C)
+Units.currency(::Stock{S,C}) where {S,C} = Units.CurrencyType(C)
