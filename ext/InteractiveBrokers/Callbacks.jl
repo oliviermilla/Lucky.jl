f8b6cde71923ce4d1082a6234056d4e47bbb0b72
Olivier Milla <olivier.milla@hey.com>
2024-06-15 16:41:09 +0200

    More Comments

--- ext/InteractiveBrokers/Callbacks.jl
+++ ext/InteractiveBrokers/Callbacks.jl
@@ -21,7 +21,7 @@ function nextValidId(ib::InteractiveBrokersObservable, orderId::Int)
 end
 
 function tickPrice(ib::InteractiveBrokersObservable, tickerId::Int, field::String, price::Union{Float64,Nothing}, size::Union{Float64,Nothing}, attrib::InteractiveBrokers.TickAttrib)
-    # data received tickPrice: 1:DELAYED_BID:-1.0
+    # data received: 1 DELAYED_BID -1.0
     mapping = ib.requestMappings[Pair(tickerId, :tickPrice)]
     subject = mapping[2]
     next!(subject, field)    
@@ -30,5 +30,6 @@ end
 function tickString(ib::InteractiveBrokersObservable, tickerId::Int, tickType::String, value::String)
     # data received: 1 DELAYED_LAST_TIMESTAMP 1718409598
     # TODO Dispatch
+    # TODO Merge with tickPrice Data !!!!
     println(tickerId, tickType, value)
 end
