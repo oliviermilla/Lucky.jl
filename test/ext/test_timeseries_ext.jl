819a008c48ba79dbeb008a2da686d11979c6f6cf
Olivier Milla <olivier.milla@hey.com>
2024-07-23 21:44:27 +0200

    More coverage

--- test/ext/test_timeseries_ext.jl
+++ test/ext/test_timeseries_ext.jl
@@ -8,4 +8,11 @@
         @test length(vector) == 2
     end
 
+    @testset "Interfaces" begin
+        instr = Stock(:AAPL, :USD)
+        qt = quotes(instr, timearray)
+        @test isa(qt, Rocket.ProxyObservable)
+        subscribe!(qt |> take(1), x -> @test isa(x, AbstractQuote))
+    end
+
 end
